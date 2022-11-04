504bbd2f30bc23b3e8abf05a305e7d7b6d3e1837
WarriorGoat <michael.d.spitz@gmail.com>
2022-11-04 12:56:19 -0400

    Revised File

--- main.js
+++ main.js
@@ -2,19 +2,8 @@
  * OUR HELPER CODE *
 *******************/
 
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- * 
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
+
+ //Here we add the squares to the canvas dynamically.
 const gridWidth = 50; //lookup CSS Grid!
 let count = 0;
 while (count <= gridWidth * gridWidth) {
@@ -37,22 +26,6 @@ buttonReset.innerText = "Reset All";
 buttonNight.innerText = "Click for Night";
 
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
-
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
-
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
 
 /***********
  * QUERIES *
@@ -127,14 +100,3 @@ buttonNight.addEventListener("click", function(){
   buttonNight.innerText = "Click for Night";
   }
 })
-
-/**************************
- * WIRING IT ALL TOGETHER *
-**************************/
-
-// Now: wiring up our event listeners to our html node elements.
-// You'll need to add the appropriate event listener for each
-// square and for each palette color from the functions you
-// wrote above.
-
-
